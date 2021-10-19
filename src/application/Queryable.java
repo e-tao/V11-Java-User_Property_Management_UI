2a70377f4c89b557b537fc17944b573c97768832
Ethan Tao <eth@ntao.ca>
2021-10-18 21:03:31 -0400

    add description/comment to interface methods

--- src/application/Queryable.java
+++ src/application/Queryable.java
@@ -9,8 +9,19 @@ public interface Queryable {
 
 	public HashMap<String, String> getTableAttribute();
 
-	// the getTableA
+	// this method is a Hard coded HashMap, it uses related instant variables
+	// (usually those include in the constructor) of the implementing Class as key
+	// and SQL query attributes as value;
+	//
+	// When the ViewGenerator.getView() is called, the getTableAttribute() inside
+	// the method returns HashTab, the values are used to generate TableColumn
+	// titles, and keys are used for PropertyValueFactory;
 
 	public TableView<Queryable> tableGenerator() throws SQLException;
+
+	// This method calls the ViewGenerator.getView() method to generate a TableView
+	// and query database to get all the results.
+	// ActionEvents in the Main Class create SecLevelWindows to hold the TableView
+	// results.
 
 }
